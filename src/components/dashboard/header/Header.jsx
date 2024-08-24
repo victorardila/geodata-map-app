37fa4cd869265d6f740a7132fa7c092f0ae3c051
Valfonsoardila10 <victoradila@gmail.com>
2024-08-23 22:03:04 -0500

    cambios de icono de notificacion

--- src/components/dashboard/header/Header.jsx
+++ src/components/dashboard/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../../assets/icon/icon.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog, faBell } from "@fortawesome/free-solid-svg-icons";
@@ -7,7 +7,13 @@ import "./Header.style.css";
 
 const Header = () => {
   const [notification, setNotification] = useState(0);
-  
+  // declaro una funcion que se cargara 4min despues de que se renderice el componente y quede asi
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNotification(notification + 1);
+    }, 120000);
+    return () => clearInterval(interval);
+  }, [notification]);
   return (
     <div className="header-state-app">
       <div className="header-state-card">
@@ -20,14 +26,25 @@ const Header = () => {
             <h3>Dashboard</h3>
           </div>
           <div className="header-cog-state">
-            {
-              notification > 0 ? (
-                <div className="notification">
-                  <img src={bellNoti} alt="Notification" />
-                </div>
-              ) : <FontAwesomeIcon icon={faBell} style={{color: "rgba(242, 185, 12)"}} />
-            }
-            <FontAwesomeIcon icon={faCog} spin style={{color: "rgba(40, 40, 40, 0.7)"}} />
+            {notification > 0 ? (
+              <div className="header-item">
+                <img src={bellNoti} alt="Notification" />
+              </div>
+            ) : (
+              <div className="header-item">
+                <FontAwesomeIcon
+                  icon={faBell}
+                  style={{ color: "rgba(242, 185, 12)" }}
+                />
+              </div>
+            )}
+            <div className="header-item">
+              <FontAwesomeIcon
+                icon={faCog}
+                spin
+                style={{ color: "rgba(40, 40, 40, 0.7)" }}
+              />
+            </div>
           </div>
         </div>
       </div>
